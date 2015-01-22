6dc40bc453416883c154c62618fa2c553d04536e
Игорь <igor@iMac-Igor.local>
2015-01-22 04:00:29 -0800

    Initial Commit

---
+++ qwer/ViewController.h
@@ -0,0 +1,13 @@
+//
+//  ViewController.h
+//  qwer
+//
+//  Created by Игорь on 22.01.15.
+//  Copyright (c) 2015 ___FULLUSERNAME___. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+@end
